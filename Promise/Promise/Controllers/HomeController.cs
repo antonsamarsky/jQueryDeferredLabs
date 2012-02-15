d726446dd8db706a307fae72ace27cd60cee4d36
asa <asa@e-conomic.com>
2012-02-15 17:24:27 +0200

    Add Home Controller and View.

---
+++ Promise/Promise/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+
+namespace Promise.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		//
+		// GET: /Home/Create
+
+		public ActionResult GetData()
+		{
+			return null;
+		}
+
+		//
+		// POST: /Home/Create
+
+		[HttpPost]
+		public ActionResult SetData(FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return null;
+			}
+		}
+
+	}
+}
