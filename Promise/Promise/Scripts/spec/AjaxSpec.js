148c7efb1ba0068070ae73eddbff1760af1a7e2f
asa <asa@e-conomic.com>
2012-02-17 15:14:27 +0200

    Add examples:fail, always, pipe.

--- Promise/Promise/Scripts/spec/AjaxSpec.js
+++ Promise/Promise/Scripts/spec/AjaxSpec.js
@@ -32,25 +32,18 @@
 		});
 	});
 
-	xdescribe("Using deferred objects", function () {
-		it("Should get user, change his name and save.", function () {
+	xdescribe("Using deferred objects: single Ajax request", function () {
+		it("Should get user.", function () {
 
 			var getUserPromise = $.getJSON("/Home/GetUser", null);
 
-			var getUserDone = getUserPromise.done(function (user) {
+			getUserPromise.done(function (user) {
 				console.log("User name: " + user.UserName);
-				user.UserName = "Alex";
-				return $.post("/Home/SaveUser", user);
-			});
-
-			getUserDone.done(function (savedUser) {
-				console.log("User has been saved: " + savedUser.UserName);
 			});
 		});
 	});
 
-
-	xdescribe("Using deferred objects", function () {
+	xdescribe("Then: Using deferred objects: two Ajax requests", function () {
 		it("Should get user and role, change them name and save.", function () {
 
 			// The promise is a read-only view into the result of the task
@@ -58,11 +51,56 @@
 			var getRolePromise = $.getJSON("/Home/GetDefaultRole", null);
 
 			jQuery.when(getUserPromise, getRolePromise).then(function (getUserArgs, getRoleArgs) {
-				console.log('I fire once BOTH ajax requests have completed!');
+				console.log("Both Ajax requests have completed!");
 				console.log("User: " + getUserArgs[0].UserName);
 				console.log("Role: " + getRoleArgs[0].RoleName);
-			}).fail(function () {
-				console.log('I fire if one or more requests failed.');
+			});
+		});
+	});
+
+	xdescribe("Fail: Using deferred objects: two Ajax requests", function () {
+		it("Should get user and role, change them name and save.", function () {
+			// The promise is a read-only view into the result of the task
+			var getUserPromise = $.getJSON("/Home/GetUser", null);
+			var invalidUrlPromise = $.getJSON("InvalidUrl", null);
+
+			jQuery.when(getUserPromise, invalidUrlPromise).fail(function () {
+				console.log("One or more requests failed.");
+			});
+		});
+	});
+
+	xdescribe("Always: Using deferred objects: two Ajax requests", function () {
+		it("Should get user and role, change them name and save.", function () {
+			// The promise is a read-only view into the result of the task
+			var getUserPromise = $.getJSON("/Home/GetUser", null);
+			var getRolePromise = $.getJSON("/Home/GetDeaultRole", null);
+			var invalidUrlPromise = $.getJSON("InvalidUrl", null);
+
+			jQuery.when(getUserPromise, getRolePromise).always(function () {
+				console.log("Always executed.");
+			});
+
+			jQuery.when(getUserPromise, invalidUrlPromise).always(function () {
+				console.log("Always executed.");
+			});
+		});
+	});
+
+	describe("Using deferred objects: pipe Ajax requests", function () {
+		it("Should get user.", function () {
+
+			var getUserPromise = $.getJSON("/Home/GetUser", null);
+
+			var pipedSaveUserPromise = getUserPromise.pipe(function (user) {
+				console.log("User name: " + user.UserName);
+
+				user.UserName = "Alex";
+				return $.post("/Home/SaveUser", user);
+			});
+
+			pipedSaveUserPromise.done(function (savedUser) {
+				console.log("User has been saved: " + savedUser.UserName);
 			});
 		});
 	});
