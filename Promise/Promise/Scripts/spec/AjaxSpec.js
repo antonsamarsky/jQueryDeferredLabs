fc87a64d96645be3ca7c4e7cc73d2f066be8c921
asa <asa@e-conomic.com>
2012-02-17 14:28:55 +0200

    DeferredSpec => AjaxSpec.
    Add on fail callback for the deferred example.

--- Promise/Promise/Scripts/spec/AjaxSpec.js
+++ Promise/Promise/Scripts/spec/AjaxSpec.js
@@ -1,4 +1,4 @@
-﻿describe("DeferredSpec.js", function () {
+﻿describe("AjaxSpec.js", function () {
 	xdescribe("Using anonymous callbacks", function () {
 		it("Should get user, change his name and save.", function () {
 
@@ -50,15 +50,19 @@
 	});
 
 
-	describe("Using deferred objects", function () {
+	xdescribe("Using deferred objects", function () {
 		it("Should get user and role, change them name and save.", function () {
 
+			// The promise is a read-only view into the result of the task
 			var getUserPromise = $.getJSON("/Home/GetUser", null);
 			var getRolePromise = $.getJSON("/Home/GetDefaultRole", null);
 
 			jQuery.when(getUserPromise, getRolePromise).then(function (getUserArgs, getRoleArgs) {
+				console.log('I fire once BOTH ajax requests have completed!');
 				console.log("User: " + getUserArgs[0].UserName);
 				console.log("Role: " + getRoleArgs[0].RoleName);
+			}).fail(function () {
+				console.log('I fire if one or more requests failed.');
 			});
 		});
 	});
