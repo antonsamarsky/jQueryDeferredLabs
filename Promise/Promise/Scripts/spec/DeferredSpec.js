0d85745478460339dc8c06ba0a415788d5d5f428
asa <asa@e-conomic.com>
2012-02-18 15:32:36 +0200

    Add simple deferred example.

--- Promise/Promise/Scripts/spec/DeferredSpec.js
+++ Promise/Promise/Scripts/spec/DeferredSpec.js
@@ -1,6 +1,17 @@
 ﻿describe("DeferredSpec.js", function () {
-	xdescribe("", function () {
-		it("", function () {
+	xdescribe("Simple deferred", function () {
+		it("Should create a simple async deferred.", function () {
+			var deferred = jQuery.Deferred();
+
+			setTimeout(function () {
+				this.resolve();
+			} .bind(deferred), 500);
+
+			deferred.done(function() {
+				console.log("It is done!");
+			});
+
+			console.log("Start doing...");
 		});
 	});
 });
