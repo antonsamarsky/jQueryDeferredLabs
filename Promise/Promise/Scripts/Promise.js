08e9184876c7034e30afb1f4bb16327b2ed8d306
asa <asa@e-conomic.com>
2012-02-18 16:36:49 +0200

    Add preparations to show .promise: http://api.jquery.com/promise/

---
+++ Promise/Promise/Scripts/Promise.js
@@ -0,0 +1,11 @@
+﻿$("button").bind("click", function () {
+	$("p").append("Started...");
+
+	$("div").each(function (i) {
+		$(this).fadeIn().fadeOut(1000 * (i + 1));
+	});
+
+	$("div").promise().done(function () {
+		$("p").append(" Finished! ");
+	});
+});
