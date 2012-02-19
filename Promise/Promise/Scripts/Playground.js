ea10f06976c99bb50c8c1286d9359613219524b4
asa <asa@e-conomic.com>
2012-02-19 07:56:05 +0200

    Add playground example.

--- Promise/Promise/Scripts/Playground.js
+++ Promise/Promise/Scripts/Playground.js
@@ -1,3 +1,11 @@
-﻿$(function() {
-	
+﻿$(function () {
+	$.doit = function () {
+		var deferred = $.Deferred();
+		setTimeout(deferred.resolve, 500);
+		return deferred.promise();
+	};
+
+	$.doit().done(function () {
+		alert("Done!");
+	});
 });
